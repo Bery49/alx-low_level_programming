33d30434d529e5a8f37df49d10026e72e6617f12
Bery49 <141265421+Bery49@users.noreply.github.com>
2023-09-18 14:06:07 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,12 +5,9 @@
  * _strlen - Calculates the length of a string.
  * @s: Pointer to a string.
  *
- * Description: This function takes a pointer to a string as a parameter
- * and calculates the length of the string (number of characters).
- *
- * Return: The length of the string.
+ * Return: The length of the string (number of characters).
  */
-void _puts(char *str);
+int _strlen(char *s)
 {
     int length = 0;
 
@@ -20,5 +17,5 @@ void _puts(char *str);
         s++;
     }
 
-    return (length); // Return statement with parentheses
+    return length;
 }
